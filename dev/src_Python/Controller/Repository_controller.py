7026738500336c5b2aef9a1e294928e1b6ec5923
Shahriar.Sh <66387131+shahriar-shahram@users.noreply.github.com>
2020-08-29 15:55:09 -0400

    Add files via upload
    Cubic_Spline_planner.py  ---> Used to interpolate and extract interpolation of the waypoints based on a constant measure
    
    Mpc_testing.py ---> Simulation made to test MPC control on python Matplotlib library graphical interface
    
    repo_mpc.py ---> This file will be called via controller class later to perform the control
    
    Repository_controller.py ---> This will be the future class of controller

---
+++ dev/src_Python/Controller/Repository_controller.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 27 19:30:36 2020
+
+@author: shahriar
+"""
+
+class VehiclePIDController():
+    """
+    VehiclePIDController is the combination of two PID controllers
+    (lateral and longitudinal) to perform the
+    low level control a vehicle from client side
+    """
+
+
+    def __init__(self, vehicle, x_positions, y_positions, time_stamps, max_throttle=0.75, max_brake=0.3, max_steering=0.8):
+        """
+        Constructor method.
+        :param vehicle: actor to apply to local planner logic onto
+        :param args_lateral: dictionary of arguments to set the lateral PID controller
+        using the following semantics:
+            K_P -- Proportional term
+            K_D -- Differential term
+            K_I -- Integral term
+        :param args_longitudinal: dictionary of arguments to set the longitudinal
+        PID controller using the following semantics:
+            K_P -- Proportional term
+            K_D -- Differential term
+            K_I -- Integral term
+        """
+
+        self.max_brake = max_brake
+        self.max_throt = max_throttle
+        self.max_steer = max_steering
+
+        self._vehicle = vehicle
+        self._world = self._vehicle.get_world()
+        self.past_steering = self._vehicle.get_control().steer
+        #self._lon_controller = PIDLongitudinalController(self._vehicle, **args_longitudinal)
+        #self._lat_controller = PIDLateralController(self._vehicle, **args_lateral)
+
+    def run_step(self, target_speed, waypoint):
+        """
+        Execute one step of control invoking both lateral and longitudinal
+        PID controllers to reach a target waypoint
+        at a given target_speed.
+            :param target_speed: desired vehicle speed
+            :param waypoint: target location encoded as a waypoint
+            :return: distance (in meters) to the waypoint
+        """
+
+        [acceleration,current_steering] = self._mpc_controller.run_step(target_speed)
+        
+        control = carla.VehicleControl()
+        if acceleration >= 0.0:
+            control.throttle = min(acceleration, self.max_throt)
+            control.brake = 0.0
+        else:
+            control.throttle = 0.0
+            control.brake = min(abs(acceleration), self.max_brake)
+
+        # Steering regulation: changes cannot happen abruptly, can't steer too much.
+
+        if current_steering > self.past_steering + 0.1:
+            current_steering = self.past_steering + 0.1
+        elif current_steering < self.past_steering - 0.1:
+            current_steering = self.past_steering - 0.1
+
+        if current_steering >= 0:
+            steering = min(self.max_steer, current_steering)
+        else:
+            steering = max(-self.max_steer, current_steering)
+
+        control.steer = steering
+        control.hand_brake = False
+        control.manual_gear_shift = False
+        self.past_steering = steering
+
+        return control
+
